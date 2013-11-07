9e774afdf2723bf00d46c1bbdab5e65fd57c41d5
Martyn Loughran <me@mloughran.com>
2013-11-07 18:15:35 +0000

    Update libPusher & Reachability dependencies
    (fixes ping-pong & reconnect bugs)

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -10,7 +10,7 @@
 #define COCOAPODS_POD_AVAILABLE_Reachability
 #define COCOAPODS_VERSION_MAJOR_Reachability 3
 #define COCOAPODS_VERSION_MINOR_Reachability 1
-#define COCOAPODS_VERSION_PATCH_Reachability 0
+#define COCOAPODS_VERSION_PATCH_Reachability 1
 
 // SocketRocket
 #define COCOAPODS_POD_AVAILABLE_SocketRocket
--- Pods/libPusher/Library/PTPusher.m
+++ Pods/libPusher/Library/PTPusher.m
@@ -309,7 +309,8 @@ NSURL *PTPusherConnectionURL(NSString *host, NSString *key, NSString *clientID, 
     }
   }
   else {
-    [self handleDisconnection:connection error:error willReconnect:NO];
+    [self handleDisconnection:connection error:error willReconnect:YES];
+    [self reconnectAfterDelay:self.reconnectDelay];
   }
 }
 
